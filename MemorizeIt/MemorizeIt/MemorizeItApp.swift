d184e9a0a12db90bae4a4f993849caa25fe0d400
BrevinB <bblalock313@gmail.com>
2024-07-08 13:21:35 -0400

    Initialized Project

---
+++ MemorizeIt/MemorizeIt/MemorizeItApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeItApp.swift
+//  MemorizeIt
+//
+//  Created by Brevin Blalock on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
